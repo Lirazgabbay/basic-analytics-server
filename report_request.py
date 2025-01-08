4f8af3596e177eab8d33514c664fcef9b882e558
Noa <noabendet@post.runi.ac.il>
2025-01-09 01:17:42 +0200

    Add: class ReportRequest for pydentic validation

---
+++ report_request.py
@@ -0,0 +1,11 @@
+"""
+Pydantic model for the request body
+
+using BaseMode class from pydantic for validates incoming request data against the model,
+raises errors for missing/incorrect fields and converts JSON into Python object.
+"""
+from pydantic import BaseModel
+
+class ReportRequest(BaseModel):
+    lastseconds: int
+    userid: str
